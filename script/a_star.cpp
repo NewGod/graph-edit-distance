9bc71094a33b477554ec9de4c202a05992f94d22
linsage980302 <40481055+linsage980302@users.noreply.github.com>
2018-06-29 14:34:57 +0800

    Update a_star.cpp

--- script/a_star.cpp
+++ script/a_star.cpp
@@ -1,3 +1,14 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+
+//using A-star algorithm to find a distance
+
+int a_star (){
+	int result;
+	
+	bool found = false;
+	while(!fouond){ 
+	}
+	return result;
+}
