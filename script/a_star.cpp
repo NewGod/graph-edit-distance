c337bb1bd8bc6b9fce8b8fd7f19375fcb28476a8
linsage980302 <40481055+linsage980302@users.noreply.github.com>
2018-06-29 17:02:35 +0800

    Update a_star.cpp

--- script/a_star.cpp
+++ script/a_star.cpp
@@ -4,10 +4,35 @@ using namespace std;
 
 //using A-star algorithm to find a distance
 
+
+class GraphChange{
+	int cost;
+	GraphChange(int cost_):cost(cost_){}
+}
+class NodeChange:public GraphChange{
+	int from, to;
+	NodeChange(int from_, int to_, int cost_):GraphChange(cost_),from(from_), to(to_){}
+}
+
+class EdgeChange:public GraphChange{
+	int from_start, from_end, to_start, to_end;
+	EdgeChange(int from_start_, int to_start_, int to_start_, int to_end_ ,int cost_):GraphChange(cost_),
+	from_start(from_start_),from_end(from_end_), to_start(to_start_), to_end(to_end_){}
+}
+
+typedef pair< vector<GraphChange*>*, int> opts;
+
+class myless {
+	public:
+	bool operator()(opts1,opts2) {
+		return opts1.second<opts2.second;
+	}
+}
+
 int a_star (){
 	bool found = false;
 	int result = 0;//graph edit distance;
-	priority_queue<> option_set;//a prioity queue that save all the options
+	priority_queue<opts, vector<opts>, myless> option_set;//a prioity queue that save all the options
 	
 	//initialize heap
 	option_set.push();
