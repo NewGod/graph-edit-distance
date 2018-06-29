f453dc1859e2a8f05ba0c55ad4bc22422160eb4e
linsage980302 <40481055+linsage980302@users.noreply.github.com>
2018-06-29 19:44:07 +0800

    Update a_star.cpp

--- script/a_star.cpp
+++ script/a_star.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
 #include<queue>
+#include<vector>
 using namespace std;
 
 //using A-star algorithm to find a distance
 
 
 class GraphChange{
+	public:
 	int cost;
 	GraphChange(int cost_):cost(cost_){}
 }
 class NodeChange:public GraphChange{
+	public:
 	int from, to;
 	NodeChange(int from_, int to_, int cost_):GraphChange(cost_),from(from_), to(to_){}
 }
 
 class EdgeChange:public GraphChange{
+	public:
 	int from_start, from_end, to_start, to_end;
 	EdgeChange(int from_start_, int to_start_, int to_start_, int to_end_ ,int cost_):GraphChange(cost_),
 	from_start(from_start_),from_end(from_end_), to_start(to_start_), to_end(to_end_){}
@@ -26,6 +30,7 @@ class opts{
 	ínt numChange;
 	int costs;
 	int imagecost;
+	int totalcost;
 }
 
 class myless {
@@ -50,17 +55,18 @@ int a_star (Graph* g1, Graph* g2){
 		opts bestopts= option_set.top();
 		option_set.pop();
 		Graph* gchange = change(g1, bestopts);
-		if judgesame(gchange, g2){
+		if (bestopts.numChange=g1.numNode){
 			found=true;
 			result=bestopts.second;
 		}
 		else {
 			opts newopts;
-			if(bestopts-><g2->numNode ) {//the final graph is smaller than the operating graph
+			if(newopts.numChange<g1.numNode ) {//the final graph is smaller than the operating graph
 				for()
 					for()
 						if()
 							option_set.push();
+				
 				option_set.push();
 			}
 			else {//the final graph has more points than the operating graph
